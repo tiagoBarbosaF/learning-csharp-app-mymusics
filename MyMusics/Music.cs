4709f2eed32d8d73061ccc7928a81ee67cf94270
Tiago Barbosa <tiago@farias.cc>
2023-06-02 19:25:41 -0300

    creating class Music.cs

---
+++ MyMusics/Music.cs
@@ -0,0 +1,28 @@
+﻿namespace MyMusics;
+
+public class Music
+{
+    public string Name { get; set; }
+    public string Artist { get; set; }
+    public int Duration { get; set; }
+    public bool Available { get; set; }
+
+    public Music(string name, string artist, int duration, bool available)
+    {
+        Name = name;
+        Artist = artist;
+        Duration = duration;
+        Available = available;
+    }
+
+    public override string ToString()
+    {
+        var textAvailable = Available ? "Available plan." : "Acquire the plus plan.";
+
+        return $"\n- Music -\n" +
+               $"Name: {Name}\n" +
+               $"Artist: {Artist}\n" +
+               $"Duration: {Duration}\n" +
+               $"Available: {textAvailable}";
+    }
+}
--- MyMusics/Program.cs
+++ MyMusics/Program.cs
@@ -1,173 +1,187 @@
-﻿const string welcomeMessage = "Welcome to My Playlist!";
-var exit = true;
-var chosenOption = "";
+﻿// #region App My Playlist
+//
+// using MyMusics;
+//
+// const string welcomeMessage = "Welcome to My Playlist!";
+// var exit = true;
+// var chosenOption = "";
+//
+// var registerBands = new Dictionary<string, List<int>>
+// {
+//   {"Millet", new List<int> {10, 9, 10, 7}},
+//   {"Beatles", new List<int> {5, 8, 10}},
+//   {"Neo Japonism", new List<int> {10, 7}},
+//   {"Doul", new List<int> {10, 9, 6, 7, 8}},
+// };
+//
+// foreach (var test in registerBands.Values)
+// {
+//   Console.WriteLine(test);
+// }
+//
+// void ShowWelcomeMessage()
+// {
+//   Console.WriteLine(@"
+// █▀▄▀█ █▄█   █▀▄▀█ █░█ █▀ █ █▀▀ █▀
+// █░▀░█ ░█░   █░▀░█ █▄█ ▄█ █ █▄▄ ▄█");
+//   Console.WriteLine();
+//   Console.WriteLine(welcomeMessage);
+// }
+//
+// void Menu()
+// {
+//   ShowWelcomeMessage();
+//   Console.WriteLine("\n- Enter 1 to register a band");
+//   Console.WriteLine("- Enter 2 to list all bands");
+//   Console.WriteLine("- Enter 3 to available a band");
+//   Console.WriteLine("- Enter 4 to show an average of a band");
+//   Console.WriteLine("- Enter 0 to exit");
+//
+//   Console.Write("\nEnter your option: ");
+//   chosenOption = Console.ReadLine();
+// }
+//
+// void OptionsMenu(string chosenOption)
+// {
+//   switch (chosenOption)
+//   {
+//     case "1":
+//       RegisterBand();
+//       break;
+//     case "2":
+//       ListBands();
+//       break;
+//     case "3":
+//       AvailableBand();
+//       break;
+//     case "4":
+//       AverageBandRate();
+//       break;
+//     case "0":
+//       exit = false;
+//       Console.WriteLine("\nSee you later!");
+//       break;
+//     default:
+//       Console.WriteLine("\nChosen Option not available!");
+//       break;
+//   }
+// }
+//
+// void RegisterBand()
+// {
+//   Console.Clear();
+//
+//   Console.WriteLine(@"
+// █▀█ █▀▀ █▀▀ █ █▀ ▀█▀ █▀▀ █▀█   █▄▄ ▄▀█ █▄░█ █▀▄
+// █▀▄ ██▄ █▄█ █ ▄█ ░█░ ██▄ █▀▄   █▄█ █▀█ █░▀█ █▄▀
+// ");
+//
+//   Console.Write($"\nEnter the band name for register: ");
+//
+//   var bandName = Console.ReadLine()!;
+//
+//   Console.WriteLine($"\nThe band {bandName} it's register successfully!");
+//
+//   registerBands.Add(bandName, new List<int>());
+//
+//   Console.ReadKey();
+// }
+//
+// void ListBands()
+// {
+//   Console.Clear();
+//
+//   Console.WriteLine(@"
+// █▄▄ ▄▀█ █▄░█ █▀▄ █▀   █░░ █ █▀ ▀█▀
+// █▄█ █▀█ █░▀█ █▄▀ ▄█   █▄▄ █ ▄█ ░█░
+// ");
+//
+//   Console.WriteLine();
+//
+//   foreach (var band in registerBands!)
+//   {
+//     Console.WriteLine($"- {band.Key}");
+//     Console.WriteLine($"  Rate: {string.Join(", ", band.Value)}\n");
+//   }
+//
+//   Console.ReadKey();
+// }
+//
+// void AvailableBand()
+// {
+//   Console.Clear();
+//
+//   Console.WriteLine(@"
+// ▄▀█ █░█ ▄▀█ █ █░░ ▄▀█ █▄▄ █░░ █▀▀   █▄▄ ▄▀█ █▄░█ █▀▄
+// █▀█ ▀▄▀ █▀█ █ █▄▄ █▀█ █▄█ █▄▄ ██▄   █▄█ █▀█ █░▀█ █▄▀
+// ");
+//
+//   Console.Write("Which band do you like to available: ");
+//
+//   var bandName = Console.ReadLine()!;
+//
+//   if (registerBands!.ContainsKey(bandName))
+//   {
+//     Console.Write($"Which rate for this band: ");
+//
+//     var rate = int.Parse(Console.ReadLine()!);
+//
+//     registerBands[bandName].Add(rate);
+//
+//     Console.WriteLine($"\nThe rate {rate} it's registered with successfully.");
+//
+//     Console.ReadKey();
+//   }
+//   else
+//   {
+//     Console.WriteLine($"Band {bandName} isn't registered.");
+//     
+//     Console.ReadKey();
+//   }
+// }
+//
+// void AverageBandRate()
+// {
+//   Console.Clear();
+//   
+//   Console.WriteLine(@"
+// ▄▀█ █░█ █▀▀ █▀█ ▄▀█ █▀▀ █▀▀   █▀█ █▀▀   ▀█▀ █░█ █▀▀   █▄▄ ▄▀█ █▄░█ █▀▄ ▀ █▀   █▀█ ▄▀█ ▀█▀ █ █▄░█ █▀▀ █▀
+// █▀█ ▀▄▀ ██▄ █▀▄ █▀█ █▄█ ██▄   █▄█ █▀░   ░█░ █▀█ ██▄   █▄█ █▀█ █░▀█ █▄▀ ░ ▄█   █▀▄ █▀█ ░█░ █ █░▀█ █▄█ ▄█
+// ");
+//   
+//   Console.Write("Which band do you like to see: ");
+//
+//   var bandName = Console.ReadLine()!;
+//   
+//   if (registerBands!.ContainsKey(bandName))
+//   {
+//     Console.Write($"Average band's rating: {registerBands[bandName].Average()}\n");
+//
+//     Console.ReadKey();
+//   }
+//   else
+//   {
+//     Console.WriteLine($"Band {bandName} isn't registered.");
+//     
+//     Console.ReadKey();
+//   }
+// }
+//
+// while (exit)
+// {
+//   Console.Clear();
+//
+//   Menu();
+//
+//   OptionsMenu(chosenOption);
+// }
+//
+// #endregion
 
-var registerBands = new Dictionary<string, List<int>>
-{
-  {"Millet", new List<int> {10, 9, 10, 7}},
-  {"Beatles", new List<int> {5, 8, 10}},
-  {"Neo Japonism", new List<int> {10, 7}},
-  {"Doul", new List<int> {10, 9, 6, 7, 8}},
-};
+using MyMusics;
 
-foreach (var test in registerBands.Values)
-{
-  Console.WriteLine(test);
-}
+var music = new Music("One Reason", "Millet", 255, true);
+var music2 = new Music("I Really Want to Stay At Your House", "Rosa Walton", 246, false);
 
-void ShowWelcomeMessage()
-{
-  Console.WriteLine(@"
-█▀▄▀█ █▄█   █▀▄▀█ █░█ █▀ █ █▀▀ █▀
-█░▀░█ ░█░   █░▀░█ █▄█ ▄█ █ █▄▄ ▄█");
-  Console.WriteLine();
-  Console.WriteLine(welcomeMessage);
-}
-
-void Menu()
-{
-  ShowWelcomeMessage();
-  Console.WriteLine("\n- Enter 1 to register a band");
-  Console.WriteLine("- Enter 2 to list all bands");
-  Console.WriteLine("- Enter 3 to available a band");
-  Console.WriteLine("- Enter 4 to show an average of a band");
-  Console.WriteLine("- Enter 0 to exit");
-
-  Console.Write("\nEnter your option: ");
-  chosenOption = Console.ReadLine();
-}
-
-void OptionsMenu(string chosenOption)
-{
-  switch (chosenOption)
-  {
-    case "1":
-      RegisterBand();
-      break;
-    case "2":
-      ListBands();
-      break;
-    case "3":
-      AvailableBand();
-      break;
-    case "4":
-      AverageBandRate();
-      break;
-    case "0":
-      exit = false;
-      Console.WriteLine("\nSee you later!");
-      break;
-    default:
-      Console.WriteLine("\nChosen Option not available!");
-      break;
-  }
-}
-
-void RegisterBand()
-{
-  Console.Clear();
-
-  Console.WriteLine(@"
-█▀█ █▀▀ █▀▀ █ █▀ ▀█▀ █▀▀ █▀█   █▄▄ ▄▀█ █▄░█ █▀▄
-█▀▄ ██▄ █▄█ █ ▄█ ░█░ ██▄ █▀▄   █▄█ █▀█ █░▀█ █▄▀
-");
-
-  Console.Write($"\nEnter the band name for register: ");
-
-  var bandName = Console.ReadLine()!;
-
-  Console.WriteLine($"\nThe band {bandName} it's register successfully!");
-
-  registerBands.Add(bandName, new List<int>());
-
-  Console.ReadKey();
-}
-
-void ListBands()
-{
-  Console.Clear();
-
-  Console.WriteLine(@"
-█▄▄ ▄▀█ █▄░█ █▀▄ █▀   █░░ █ █▀ ▀█▀
-█▄█ █▀█ █░▀█ █▄▀ ▄█   █▄▄ █ ▄█ ░█░
-");
-
-  Console.WriteLine();
-
-  foreach (var band in registerBands!)
-  {
-    Console.WriteLine($"- {band.Key}");
-    Console.WriteLine($"  Rate: {string.Join(", ", band.Value)}\n");
-  }
-
-  Console.ReadKey();
-}
-
-void AvailableBand()
-{
-  Console.Clear();
-
-  Console.WriteLine(@"
-▄▀█ █░█ ▄▀█ █ █░░ ▄▀█ █▄▄ █░░ █▀▀   █▄▄ ▄▀█ █▄░█ █▀▄
-█▀█ ▀▄▀ █▀█ █ █▄▄ █▀█ █▄█ █▄▄ ██▄   █▄█ █▀█ █░▀█ █▄▀
-");
-
-  Console.Write("Which band do you like to available: ");
-
-  var bandName = Console.ReadLine()!;
-
-  if (registerBands!.ContainsKey(bandName))
-  {
-    Console.Write($"Which rate for this band: ");
-
-    var rate = int.Parse(Console.ReadLine()!);
-
-    registerBands[bandName].Add(rate);
-
-    Console.WriteLine($"\nThe rate {rate} it's registered with successfully.");
-
-    Console.ReadKey();
-  }
-  else
-  {
-    Console.WriteLine($"Band {bandName} isn't registered.");
-    
-    Console.ReadKey();
-  }
-}
-
-void AverageBandRate()
-{
-  Console.Clear();
-  
-  Console.WriteLine(@"
-▄▀█ █░█ █▀▀ █▀█ ▄▀█ █▀▀ █▀▀   █▀█ █▀▀   ▀█▀ █░█ █▀▀   █▄▄ ▄▀█ █▄░█ █▀▄ ▀ █▀   █▀█ ▄▀█ ▀█▀ █ █▄░█ █▀▀ █▀
-█▀█ ▀▄▀ ██▄ █▀▄ █▀█ █▄█ ██▄   █▄█ █▀░   ░█░ █▀█ ██▄   █▄█ █▀█ █░▀█ █▄▀ ░ ▄█   █▀▄ █▀█ ░█░ █ █░▀█ █▄█ ▄█
-");
-  
-  Console.Write("Which band do you like to see: ");
-
-  var bandName = Console.ReadLine()!;
-  
-  if (registerBands!.ContainsKey(bandName))
-  {
-    Console.Write($"Average band's rating: {registerBands[bandName].Average()}\n");
-
-    Console.ReadKey();
-  }
-  else
-  {
-    Console.WriteLine($"Band {bandName} isn't registered.");
-    
-    Console.ReadKey();
-  }
-}
-
-while (exit)
-{
-  Console.Clear();
-
-  Menu();
-
-  OptionsMenu(chosenOption);
-}
+Console.WriteLine(music);
+Console.WriteLine(music2);
